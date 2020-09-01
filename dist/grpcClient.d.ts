27a2f345623cde13412b50864013c2ef4bdd06e5
Adam Soltys <asoltys@gmail.com>
2020-08-31 21:28:10 -0700

    dist folder

---
+++ dist/grpcClient.d.ts
@@ -0,0 +1,24 @@
+import * as services from 'tdex-protobuf/js/trade_grpc_pb';
+import TraderClientInterface from './grpcClientInterface';
+export declare class TraderClient implements TraderClientInterface {
+    providerUrl: string;
+    client: services.TradeClient;
+    constructor(providerUrl: string);
+    /**
+     * tradePropose
+     * @param market
+     * @param tradeType
+     * @param swapRequestSerialized
+     */
+    tradePropose({ baseAsset, quoteAsset }: any, tradeType: number, swapRequestSerialized: Uint8Array): Promise<any>;
+    /**
+     * tradeComplete
+     * @param swapCompleteSerialized
+     */
+    tradeComplete(swapCompleteSerialized: Uint8Array): Promise<any>;
+    markets(): Promise<Array<any>>;
+    balances({ baseAsset, quoteAsset, }: {
+        baseAsset: string;
+        quoteAsset: string;
+    }): Promise<any>;
+}
---
+++ dist/grpcClientInterface.d.ts
@@ -0,0 +1,11 @@
+export default interface TraderClientInterface {
+    providerUrl: string;
+    client: any;
+    tradePropose({ baseAsset, quoteAsset }: any, tradeType: number, swapRequestSerialized: Uint8Array): Promise<any>;
+    tradeComplete(swapCompleteSerialized: Uint8Array): Promise<any>;
+    markets(): Promise<Array<any>>;
+    balances({ baseAsset, quoteAsset, }: {
+        baseAsset: string;
+        quoteAsset: string;
+    }): Promise<any>;
+}
